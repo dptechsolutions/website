f4fe87727c5091156888504ec08c98241c6218cc
Tejasvi S Jain <tejasvisjain0214@gmail.com>
2023-03-14 00:11:40 +0530

    Assests

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/NavBar'
+import Portfolio from './components/Portfolio';
+import SocialLinks from './components/SocialLinks';
+import Experience from './components/Experience'
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <SocialLinks />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import {FaGithub, FaLinkedin} from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
+function SocialLinks() {
+
+  const links =[
+    {
+      id: 1,
+      child:(
+            <>
+              Linkedin <FaLinkedin  size={30}/>
+            </>
+      ),
+      href: "https://www.linkedin.com/in/tejasvisjain/",
+      style: 'rounded-tr-md'
+    },
+    {
+      id: 2,
+      child:(
+            <>
+              Github <FaGithub  size={30}/>
+            </>
+      ),
+      href: "https://www.github.com/TEJASVISJAIN/",
+    },
+    {
+      id: 3,
+      child:(
+            <>
+              Mail <HiOutlineMail  size={30}/>
+            </>
+      ),
+      href: "mailto:tejasvisjain0214@gmail.com"
+    },
+    {
+      id: 4,
+      child:(
+            <>
+              Resume <BsFillPersonLinesFill  size={30}/>
+            </>
+      ),
+      href: "/resume.pdf",
+      style: 'rounded-br-md',
+      download: true
+    }
+  ]
+
+  return (
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {links.map(({id,href,child,style,download}) => (
+          // eslint-disable-next-line
+          <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 bg-gray-500 hover:rounded-md ml-[-100px] hover:ml-[-10px] duration-300" + " " + style}>
+          <a 
+            download={download} 
+            target ='_blank'
+            rel='noreferrer'
+            href={href} 
+            className="flex justify-between items-center w-full text-white ">
+            {child}
+          </a>
+        </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default SocialLinks
